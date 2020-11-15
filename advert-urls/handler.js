600b7280d78f82a9c80d7ccad0fa1d021b891e93
Alan Ip <theipster@users.noreply.github.com>
2020-11-15 07:16:29 +0000

    🔌 Implement URL decoder for Zoopla sale adverts.
    Decoded identifiers are pushed onto SNS for further processing.

--- advert-urls/handler.js
+++ advert-urls/handler.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+const sns = new AWS.SNS();
+
+const topicArns = {
+  ZOOPLA_SALE: process.env.ZOOPLA_SALE_TOPIC_ARN
+};
+
+function generateNotification(url, topicArns) {
+  let zooplaSaleMatches = /^https:\/\/www\.zoopla\.co\.uk\/for-sale\/details\/([0-9]+)/.exec(url);
+  if (zooplaSaleMatches) {
+    return {
+      Message: "Detected Zoopla Sale advert",
+      MessageAttributes: {
+        id: {
+          DataType: "String",
+          StringValue: zooplaSaleMatches[1]
+        }
+      },
+      TopicArn: topicArns.ZOOPLA_SALE
+    };
+  }
+
+  throw new Error("Could not recognise URL: " + url);
+}
+
 module.exports.decode = async event => {
-  console.log(JSON.stringify(event));
+  let url = event.Records[0].dynamodb.Keys.url.S;
+  let notification = generateNotification(url, topicArns);
+  return sns.publish(notification).promise();
 };
