a23de55fe3ab094fc65e7895d75b92cefafd0bfd
Alan Ip <theipster@users.noreply.github.com>
2020-11-23 03:59:14 +0000

    📷 Zoopla sale adverts: take snapshot and persist to S3 for future processing.

--- zoopla-sale-adverts/handler.js
+++ zoopla-sale-adverts/handler.js
@@ -1,5 +1,72 @@
 'use strict';
 
+const AWS = require("aws-sdk"),
+  crypto = require("crypto"),
+  https = require("https");
+
+const env = process.env;
+const s3 = new AWS.S3();
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`Download failed: HTTP ${response.statusCode}.`);
+    this.errorType = "HttpError";
+    this.httpResponseHeaders = response.headers;
+  }
+}
+
+function httpsGet(url) {
+  return new Promise((resolve, reject) => {
+    console.log(`Downloading URL ${url}`);
+
+    https.get(
+      url,
+      response => {
+        if (response.statusCode != 200) {
+          response.resume();
+          throw new HttpError(response);
+        }
+
+        let body = "";
+        response.on(
+          "data",
+          data => {
+            body += data;
+          }
+        );
+
+        response.on(
+          "end",
+          () => {
+            console.log(`Downloaded ${Buffer.byteLength(body)} bytes.`);
+            resolve(body);
+          }
+        );
+      }
+    ).on(
+      "error",
+      reject
+    ).end();
+  });
+}
+
+function putS3(id, body, bucket) {
+  let contentMd5 = crypto.createHash("md5")
+    .update(body)
+    .digest("base64");
+
+  return s3.putObject({
+    Body: body,
+    Bucket: bucket,
+    ContentMD5: contentMd5,
+    ContentType: "text/html",
+    Key: `details/${id}.html`
+  }).promise();
+}
+
 module.exports.capture = async event => {
-  console.log(JSON.stringify(event));
+  let id = event.detail.id;
+
+  return httpsGet(`https://www.zoopla.co.uk/for-sale/details/${id}`)
+    .then(body => putS3(id, body, env.BUCKET));
 };
