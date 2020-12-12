baedf9fb0fe9b0cad3985ea3a11ce7d869e42ba0
Alan Ip <theipster@users.noreply.github.com>
2020-12-12 17:54:14 +0000

    🚀 Serverless: add deployment script. (#17)
    * 🚀 Serverless: hacky script to deploy all services in parallel.
    
    * 🚀 Serverless: store logs in the base directory only.
    
    Moving the logs out of the individual service directories prevents
    Serverless from accidentally including the start of the logs when
    building the deployment artifacts.
    
    * 🚀 Serverless: add deploy command.
    
    Also added instructions on README.md.
    
    * 🚀 Serverless: add error handling to deployment script.

---
+++ deploy-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+now=`date +%Y%m%d-%H%M%S`
+basedir=`dirname "$0"`
+
+# Ensure log dir exists
+logdir="$basedir/logs/deployment-$now"
+mkdir -p $logdir
+logdir=`realpath "$logdir"`
+
+# Run serverless deploy
+find "$basedir" -type f -name "serverless.yml" -exec dirname {} \; \
+    | xargs -I {} -P 0 bash --login -O expand_aliases -c "cd {}; service=\$(basename {}); echo \"Deploying \$service...\"; serverless deploy $@ > \"$logdir/\$service.log\"" \
+    && echo "Deployed. 🚀" \
+    || echo "Deployment failed. Logs: $logdir"
