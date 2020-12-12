6f6f62706ffc8657bd68f6d9f3973e6057755445
Alan Ip <theipster@users.noreply.github.com>
2020-12-12 17:51:13 +0000

    🚀 Serverless: add error handling to deployment script.

--- deploy-all.sh
+++ deploy-all.sh
@@ -11,4 +11,5 @@ logdir=`realpath "$logdir"`
 # Run serverless deploy
 find "$basedir" -type f -name "serverless.yml" -exec dirname {} \; \
     | xargs -I {} -P 0 bash --login -O expand_aliases -c "cd {}; service=\$(basename {}); echo \"Deploying \$service...\"; serverless deploy $@ > \"$logdir/\$service.log\"" \
-    && echo "Deployed. 🚀"
+    && echo "Deployed. 🚀" \
+    || echo "Deployment failed. Logs: $logdir"
